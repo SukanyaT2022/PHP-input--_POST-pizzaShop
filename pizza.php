aaf6ccea36e10ac1d49d7827162c8022993d7218
TiparpronS <Sukanyat2022a@gmail.com>
2023-02-03 15:35:15 -0500

    first

---
+++ pizza.php
@@ -0,0 +1,10 @@
+
+<?php
+// <!-- first we read name from html [inside name= from html page] -->
+$crust = $_POST["crust"];
+$topping = $_POST["top"];
+$quantity = $_POST["qty"];
+$sizePizza =  $_POST["size"];
+//print eco and . for join
+echo "Pizza Crust" . $crust;
+?>
